8513a791cf042db7a6705cf13977f1954efa27a9
Linhao FAN <96882555+fsxlike@users.noreply.github.com>
2023-09-04 14:09:07 +0800

    Create TU05 IF statements.py

---
+++ TU05 IF statements.py
@@ -0,0 +1,5 @@
+test = input("Is it raining? y/n ")
+if test in ["y","Y","Yes","yes"]:
+    print("Oh dear, no football today!")
+else: print("Great we can play!")
+# any character input except for "y" will be considered as no
