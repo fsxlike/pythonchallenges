8ed502f0da8266928e98cb39b7a318803c2e88ed
Linhao FAN <96882555+fsxlike@users.noreply.github.com>
2023-09-05 13:49:57 +0800

    Create TU07 Simple for loops.py

---
+++ TU07 Simple for loops.py
@@ -0,0 +1,14 @@
+#Exercise 1
+for i in range(0,101):
+    print(i)
+
+#Exercise 2
+count = 0
+for a in range(0,101):
+    if a % 2 == 0:
+        count = count+1
+print(count)
+
+#Exercise 3
+for b in range(1000,-1,-10):
+    print(b)
