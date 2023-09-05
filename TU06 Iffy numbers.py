d93fefea8ccc954aced3fa3c2f699b50ae5165d0
Linhao FAN <96882555+fsxlike@users.noreply.github.com>
2023-09-05 13:27:11 +0800

    Create TU06 Iffy numbers.py

---
+++ TU06 Iffy numbers.py
@@ -0,0 +1,8 @@
+MyHeight = 178
+YourHeight = float(input("What's your height in cm?"))
+if MyHeight > YourHeight:
+    print("I am taller than you lol")
+elif MyHeight == YourHeight:
+    print("We are same tall!")
+else:
+    print("You are taller than me!")
